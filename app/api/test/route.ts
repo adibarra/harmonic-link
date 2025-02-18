da10acfc0b1ea60566bfa37bfc7ad22c58eb684e
Kevin Dang <KXD200014@utdallas.edu>
2025-02-18 11:21:16 -0600

    added access token function

---
+++ app/api/test/route.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server";
+import { topArtists } from "@/lib/spotify";
+
+
+export const GET = async () => {
+
+  const data = await topArtists();
+
+
+  return  NextResponse.json(data.artists.items);
+}
