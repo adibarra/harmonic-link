2bd45465957f59ec12a9377f457ccd53eab1b25c
Kevin Dang <KXD200014@utdallas.edu>
2025-02-28 12:11:29 -0600

    Added functions to obtain spotify data

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,11 +1,13 @@
 import { NextResponse } from "next/server";
-import { topArtists } from "@/lib/spotify";
+import { getAlbumArtists, getArtistAlbums, getArtist } from "@/lib/spotify";
 
 
 export const GET = async () => {
 
-  const data = await topArtists();
 
+   //const data = await getArtist("7tYKF4w9nC0nq9CsPZTHyP");
+  const data = await getAlbumArtists("76290XdXVF9rPzGdNRWdCh");
 
-  return  NextResponse.json(data.artists.items);
+  return  NextResponse.json(data);
 }
+
