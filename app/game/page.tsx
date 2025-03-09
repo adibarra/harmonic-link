83cc5975007af737324a662df7eac87928eb67d6
adibarra <93070681+adibarra@users.noreply.github.com>
2025-03-08 20:45:11 -0600

    overhaul game page to use new logic

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,134 +1,117 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { Input } from "@/components/ui/input";
 import { MoonLoader } from "react-spinners";
-import SearchDropdown from "@/components/search-dropdown";
 import ArtistCard from "@/components/game/artist-card";
 import ChainDisplay from "@/components/game/chain-display";
-import useDebounce from "../hooks/useDebounce";
-
-interface Artist {
-  id: string;
-  name: string;
-  image: string;
-}
-
-interface Album {
-  id: string;
-  name: string;
-  artist: string;
-  image: string;
-}
+import { fetchArtistData } from "@/services/fetchArtistData";
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
+import { fetchAlbums } from "@/services/fetchAlbums";
+import { fetchAlbumArtists } from "@/services/fetchAlbumArtists";
+import Image from "next/image";
 
 export default function HarmonicLinks() {
-  const [startArtist] = useState<Artist>({
-    id: "000",
-    name: "Imagine Dragons",
-    image: "https://i.scdn.co/image/ab67616100005174ab47d8dae2b24f5afe7f9d38",
-  });
-  const [endArtist] = useState<Artist>({
-    id: "001",
-    name: "Taylor Swift",
-    image: "https://i.scdn.co/image/ab67616100005174e672b5f553298dcdccb0e676",
-  });
-  const [searchTerm, setSearchTerm] = useState("");
-  const [albums, setAlbums] = useState<Album[]>([]);
-  const [linkChain, setLinkChain] = useState<Album[]>([]);
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const [startArtist, setStartArtist] = useState<Artist | null>(null);
+  const [endArtist, setEndArtist] = useState<Artist | null>(null);
+  const [items, setItems] = useState<ChainItem[]>([]);
+  const [linkChain, setLinkChain] = useState<ChainItem[]>([]);
   const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState(false);
 
-  const debouncedSearchTerm = useDebounce(searchTerm, 300);
+  useEffect(() => {
+    const fetchArtists = async () => {
+      const startArtistId = searchParams.get("start");
+      const endArtistId = searchParams.get("end");
+
+      if (startArtistId && endArtistId) {
+        try {
+          const fetchedStartArtist = await fetchArtistData(startArtistId);
+          const fetchedEndArtist = await fetchArtistData(endArtistId);
+
+          setStartArtist(fetchedStartArtist);
+          setEndArtist(fetchedEndArtist);
+          setItems((await fetchAlbums(startArtistId)) || []);
+        } catch (error) {
+          console.error("Error fetching artist data:", error);
+        }
+      } else {
+        router.push("/loading");
+      }
+    };
+
+    fetchArtists();
+  }, [searchParams]);
 
   useEffect(() => {
-    if (debouncedSearchTerm) {
-      handleSearch();
-    }
-  }, [debouncedSearchTerm]);
-
-  const fetchAlbums = async (query: string): Promise<Album[]> => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve([
-          {
-            id: "1",
-            name: "LOOM",
-            artist: "Imagine Dragons",
-            image: "https://placehold.co/150/png",
-          },
-          {
-            id: "2",
-            name: "Loosing",
-            artist: "Groove Chorus",
-            image: "https://placehold.co/150/png",
-          },
-          {
-            id: "3",
-            name: "Am I Still Dreaming?",
-            artist: "Loose Room",
-            image: "https://placehold.co/150/png",
-          },
-          {
-            id: "4",
-            name: "Totally Accountable",
-            artist: "LOOM BAND$",
-            image: "https://placehold.co/150/png",
-          },
-        ]);
-      }, 500);
-    });
-  };
-
-  const handleSearch = async () => {
-    try {
-      setError(null);
+    const getItems = async () => {
+      if (linkChain.length === 0) return;
       setLoading(true);
-      const results = await fetchAlbums(searchTerm);
-      setAlbums(results);
-    } catch (err) {
-      setError("Failed to fetch albums. Please try again.");
-    } finally {
-      setLoading(false);
-    }
-  };
+      try {
+        const lastItem = linkChain[linkChain.length - 1];
+        if ("artist" in lastItem) {
+          const fetchedArtists = await fetchAlbumArtists(lastItem.id);
+          setItems(fetchedArtists || []);
+        } else {
+          const fetchedAlbums = await fetchAlbums(lastItem.id);
+          setItems(fetchedAlbums || []);
+        }
+        setLoading(false);
+      } catch (error) {
+        setError("Error fetching data");
+        setLoading(false);
+      }
+    };
 
-  const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchTerm(e.target.value);
-  };
+    getItems();
+  }, [linkChain]);
 
-  const addToChain = (album: Album) => {
-    setLinkChain([...linkChain, album]);
-    setSearchTerm("");
-    setAlbums([]);
+  const addToChain = async (item: Album | Artist) => {
+    setLinkChain((prev) => [...prev, item]);
   };
 
   return (
     <div className="p-6 flex flex-col items-center space-y-6 h-[90vh]">
       <h1 className="text-3xl font-bold mb-6">Harmonic Links</h1>
       <div className="flex items-center space-x-6">
-        <ArtistCard artist={startArtist} />
+        {startArtist && <ArtistCard artist={startArtist} />}
         <span className="text-xl">➡</span>
         <ChainDisplay chain={linkChain} />
         <span className="text-xl">➡</span>
-        <ArtistCard artist={endArtist} />
+        {endArtist && <ArtistCard artist={endArtist} />}
       </div>
 
-      <div className="relative w-full max-w-md">
-        <Input
-          className="w-full p-2"
-          placeholder="Enter an album's name"
-          value={searchTerm}
-          onChange={onInputChange}
-          aria-label="Album Search"
-        />
-        {loading && (
-          <div className="absolute top-0 right-0 p-2">
-            <MoonLoader size={18} color="#fff" />
-          </div>
-        )}
-        {albums.length > 0 && <SearchDropdown albums={albums} onSelect={addToChain} />}
-        {error && <p className="text-red-500 mt-2">{error}</p>}
-      </div>
+      {loading && <MoonLoader size={18} color="#fff" />}
+      {error && <p className="text-red-500 mt-2">{error}</p>}
+
+      {!loading && !error && (
+        <div className="w-full max-w-md overflow-x-auto border border-white rounded-lg">
+          <table className="min-w-full border border-gray-300 rounded-lg">
+            <tbody>
+              {items.map((item) => (
+                <tr
+                  key={item.id}
+                  className="cursor-pointer hover:bg-white hover:bg-opacity-10 border-b border-gray-300"
+                  onClick={() => addToChain(item)}
+                >
+                  <td className="py-2 px-4 flex items-center">
+                    <Image
+                      src={item.image}
+                      alt={item.name}
+                      width={48}
+                      height={48}
+                      className="rounded-lg mr-8"
+                    />
+                    {"artist" in item ? item.name : item.name.toUpperCase()}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 }
