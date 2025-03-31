9d760ea374a0e4d0d76d5962ba6aadd0667e69c9
Harthroth <nate_christie@yahoo.com>
2025-03-31 14:54:16 -0500

    added endless routes

--- app/api/game/route.ts
+++ app/api/game/route.ts
@@ -1,5 +1,6 @@
 import {NextResponse} from "next/server"
 import * as spot from "@/lib/spotify";
+import * as logic from "@/lib/logic";
 
 export async function GET(request: Request) {
 
@@ -22,6 +23,44 @@ export async function GET(request: Request) {
       case 'artistAlbums':
         const albumData = await spot.getArtistAlbums(id)
         return NextResponse.json(albumData, {status: 200});
+
+      default:
+        return NextResponse.json(
+          { error: 'Invalid type parameter' },
+          { status: 400 }
+        );
+
+    }
+
+
+
+  } catch (error) {
+    console.error('Error in get request:' , error);
+    return NextResponse.json(
+      { error: 'Internal server error' },
+      {status: 500}
+    );
+  }
+
+
+}
+export async function ENDLESS_INIT(request: Request) {
+
+  try {
+    const { searchParams } = new URL(request.url);
+
+    const id = searchParams.get('ID'); // ID = genre
+    const type = searchParams.get('type');
+
+    switch(type) {
+
+      case 'artist-artist':
+        const artist = await logic.getValidArtistStartEnd(id);
+        return NextResponse.json(artist, {status: 200});
+
+      case 'album-album':
+        const album = await logic.getValidAlbumStartEnd(id);
+        return NextResponse.json(album, {status: 200});
 
       default:
         return NextResponse.json(
