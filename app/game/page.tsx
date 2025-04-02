a0fb043cceea1bd2622ded0ae956d5ca8e5e7022
adibarra <93070681+adibarra@users.noreply.github.com>
2025-04-02 13:10:05 -0500

    add animations to loading and game components

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -3,22 +3,39 @@
 import { useState } from "react";
 import LoadingGame from "@/components/game/loading-game";
 import Game from "@/components/game/game";
+import { motion, AnimatePresence } from "motion/react";
 
 export default function HarmonicLinks() {
   const [gameReady, setGameReady] = useState(false);
   const [startChainItem, setStartChainItem] = useState<ChainItem>();
   const [endChainItem, setEndChainItem] = useState<ChainItem>();
 
+  const fadeInOut = {
+    initial: { opacity: 0 },
+    animate: { opacity: 1 },
+    exit: { opacity: 0 },
+    transition: { duration: 0.5 },
+  };
+
   if (!gameReady) {
-    return <LoadingGame onSuccess={
-      (start, end) => {
-        setStartChainItem(start);
-        setEndChainItem(end);
-        setGameReady(true);
-      }} />;
+    return (
+      <motion.div {...fadeInOut}>
+        <LoadingGame
+          onSuccess={(start, end) => {
+            setStartChainItem(start);
+            setEndChainItem(end);
+            setGameReady(true);
+          }}
+        />
+      </motion.div>
+    );
   }
 
   return (
-    <Game start={startChainItem!} end={endChainItem!} />
+    <AnimatePresence>
+      <motion.div key="game" {...fadeInOut}>
+        <Game start={startChainItem!} end={endChainItem!} />
+      </motion.div>
+    </AnimatePresence>
   );
 }
