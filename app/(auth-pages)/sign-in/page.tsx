edaee00026b84e8db73a1146c5893e719fde3d32
adibarra <93070681+adibarra@users.noreply.github.com>
2025-04-08 14:29:05 -0500

    Merge remote-tracking branch 'origin/login-page'

--- app/(auth-pages)/sign-in/page.tsx
+++ app/(auth-pages)/sign-in/page.tsx
@@ -1,44 +1,36 @@
-import { signInAction } from "@/app/actions";
-import { FormMessage, Message } from "@/components/form-message";
-import { SubmitButton } from "@/components/submit-button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import Link from "next/link";
+"use client"
 
-export default async function Login(props: { searchParams: Promise<Message> }) {
-  const searchParams = await props.searchParams;
-  return (
-    <form className="flex-1 flex flex-col min-w-64">
-      <h1 className="text-2xl font-medium">Sign in</h1>
-      <p className="text-sm text-foreground">
-        Don't have an account?{" "}
-        <Link className="text-foreground font-medium underline" href="/sign-up">
-          Sign up
-        </Link>
-      </p>
-      <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
-        <Label htmlFor="email">Email</Label>
-        <Input name="email" placeholder="you@example.com" required />
-        <div className="flex justify-between items-center">
-          <Label htmlFor="password">Password</Label>
-          <Link
-            className="text-xs text-foreground underline"
-            href="/forgot-password"
-          >
-            Forgot Password?
-          </Link>
-        </div>
-        <Input
-          type="password"
-          name="password"
-          placeholder="Your password"
-          required
-        />
-        <SubmitButton pendingText="Signing In..." formAction={signInAction}>
-          Sign in
-        </SubmitButton>
-        <FormMessage message={searchParams} />
-      </div>
-    </form>
-  );
+import { createClient } from "@/utils/supabase/client";
+import { create } from "lodash";
+
+const handleLogin = async () => {
+const supabase = await createClient()
+const { data, error } = await supabase.auth.signInWithOAuth({
+provider: 'spotify',
+  options: {
+  redirectTo: 'http://localhost:3000/api/auth/callback?next=/', // or your deployed URL
+  scopes: 'user-read-email user-read-private',
+  },
+});
+
+if (error) {
+  console.error('OAuth error:', error);
+} else {
+  console.log('Redirecting to Spotify...');
+  }
+};
+
+export default function LoginPage() {
+  return (
+  <main className="flex flex-col items-center min-h-screen gap-4">
+  <h1 className="text-2xl font-bold">Sign In</h1>
+
+    <button
+      onClick={handleLogin}
+      className="bg-green-600 text-white px-6 py-3 rounded-full hover:bg-green-700"
+  >
+  Sign in with Spotify
+  </button>
+  </main>
+);
 }
