e6a6ffbf33456f5f6c6136856a3679439e736129
adibarra <93070681+adibarra@users.noreply.github.com>
2025-04-16 13:52:22 -0500

    remove unused remote image pattern

--- next.config.ts
+++ next.config.ts
@@ -8,10 +8,6 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "i.scdn.co",
       },
-      {
-        protocol: "https",
-        hostname: "placehold.co",
-      },
     ],
   }
 };
