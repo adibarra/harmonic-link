29a3258da2b71d4ec639b484ba768c78cdc6a957
adibarra <93070681+adibarra@users.noreply.github.com>
2025-04-30 17:56:12 -0500

    refactor cards, improve card animations

--- components/display/item-card.tsx
+++ components/display/item-card.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import AlbumCard from "./album-card";
-import ArtistCard from "./artist-card";
+import { motion, AnimatePresence } from "framer-motion";
+import { Card, CardContent } from "@/components/ui/card";
+import { DiscIcon, MicIcon } from "lucide-react";
 
 interface ItemCardProps {
   item: ChainItem | null;
@@ -11,6 +12,51 @@ const isAlbum = (item: ChainItem): item is Album => {
 };
 
 export default function ItemCard({ item }: ItemCardProps) {
-  if (!item) return null;
-  return isAlbum(item) ? <AlbumCard album={item} /> : <ArtistCard artist={item} />;
-};
+  const fadeInOut = {
+    initial: { opacity: 0 },
+    animate: { opacity: 1 },
+    exit: { opacity: 0 },
+    transition: { duration: 0.5 },
+  };
+
+  return (
+    <>
+      {item && (
+        <motion.div key={item.id} {...fadeInOut}>
+          <Card className="relative w-[192px] h-[218px] p-4 pb-6">
+            <CardContent className="flex flex-col items-center text-center text-sm p-0">
+              <div className="w-[128px] h-[128px]">
+                <img
+                  className="w-full h-full object-cover"
+                  src={item.image}
+                  alt=""
+                  width={128}
+                  height={128}
+                />
+              </div>
+              <div className="mt-2 font-semibold text-base truncate w-full">{item.name}</div>
+              {isAlbum(item) && (
+                <div className="text-xs font-semibold opacity-50 truncate w-full">
+                  {item.artist}
+                </div>
+              )}
+            </CardContent>
+            <div className="absolute flex flex-row gap-2 left-2 bottom-2 opacity-50">
+              {isAlbum(item) ? (
+                <>
+                  <DiscIcon className="w-4 h-4" />
+                  <span className="text-xs">Album</span>
+                </>
+              ) : (
+                <>
+                  <MicIcon className="w-4 h-4" />
+                  <span className="text-xs">Artist</span>
+                </>
+              )}
+            </div>
+          </Card>
+        </motion.div>
+      )}
+    </>
+  );
+}
--- components/display/player-card.tsx
+++ components/display/player-card.tsx
@@ -1,7 +1,6 @@
-"use client";
-
 import { UserIcon } from "lucide-react";
 import { Card, CardContent } from "../ui/card";
+import { motion } from "motion/react";
 
 interface PlayerCardProps {
   user: User;
@@ -9,25 +8,34 @@ interface PlayerCardProps {
 }
 
 export default function PlayerCard({ user, you }: PlayerCardProps) {
+  const fadeInOut = {
+    initial: { opacity: 0 },
+    animate: { opacity: 1 },
+    exit: { opacity: 0 },
+    transition: { duration: 0.5 },
+  };
+
   return (
-    <Card className="relative w-[192px] h-[218px] p-4 pb-6">
-      <CardContent className="flex flex-col items-center text-center text-sm p-0">
-        <div className="w-[128px] h-[128px]">
-          <img
-            className="w-full h-full object-cover"
-            src={user.image}
-            alt={user.name}
-            width={128}
-            height={128}
-          />
+    <motion.div {...fadeInOut}>
+      <Card className="relative w-[192px] h-[218px] p-4 pb-6">
+        <CardContent className="flex flex-col items-center text-center text-sm p-0">
+          <div className="w-[128px] h-[128px]">
+            <img
+              className="w-full h-full object-cover"
+              src={user.image}
+              alt=""
+              width={128}
+              height={128}
+            />
+          </div>
+          <div className="mt-2 font-semibold text-base truncate w-full">{user.name}</div>
+          <div className="text-xs font-semibold opacity-50 truncate w-full">{you ? '(You)' : ''}</div>
+        </CardContent>
+        <div className="absolute flex flex-row gap-2 left-2 bottom-2 opacity-50">
+          <UserIcon className="w-4 h-4" />
+          <span className="text-xs">Player</span>
         </div>
-        <div className="mt-2 font-semibold text-base truncate w-full">{user.name}</div>
-        <div className="text-xs font-semibold opacity-50 truncate w-full">{you ? '(You)' : ''}</div>
-      </CardContent>
-      <div className="absolute flex flex-row gap-2 left-2 bottom-2 opacity-50">
-        <UserIcon className="w-4 h-4" />
-        <span className="text-xs">Player</span>
-      </div>
-    </Card>
+      </Card>
+    </motion.div>
   );
 }
